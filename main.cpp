af32dd4d89b6585c577acefa36eed1ac50e34d32
RageGen <zamorin.daniil@mail.ru>
2022-03-03 21:17:05 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include "Product.cpp"
 using namespace std;
+int ProductAmount;
 Product AddProduct()
 {
     int64_t ID;
     int64_t MeasurementUnits;
     int64_t ProductCost;
-    std::string Title;
-    std::string Color;
-    std::string MeasurementType;
+    string Title;
+    string Color;
+    string MeasurementType;
     std::cout << "Введите артикул ---> ";
     std::cin >> ID;
+    cin.ignore();
     std::cout << "Введите наименование ---> ";
-    std::cin >> Title;
+    std::getline(cin, Title);
     std::cout << "Введите цвет ---> ";
     std::cin >> Color;
     std::cout << "Введите тип единиц измерения ---> ";
@@ -24,15 +26,15 @@ Product AddProduct()
 }
 int main()
 {
-    int ProductAmount = 0;
+    vector<Product> ProductList;
+    cout << "Сколько товаров хотите добавить?" << endl;
     cin >> ProductAmount;
-    Product *ProductList = new Product[ProductAmount];
-    for (int i=0;i<ProductAmount;i++)
+    for (int i = 0; i < ProductAmount; i++)
     {
-        ProductList[i]=AddProduct();
+        ProductList.push_back(AddProduct());
     }
-    for (int i=0;i<ProductAmount;i++)
+    for (int i = 0; i < ProductAmount; i++)
     {
-       cout<<ProductList[i].m_OutProdcutInfo()<<endl;
+        ProductList[i].m_OutProdcutInfo();
     }
 }
